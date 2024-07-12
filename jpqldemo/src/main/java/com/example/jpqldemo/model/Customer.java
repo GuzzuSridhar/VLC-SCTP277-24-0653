4ced79f83f6e4b171210b81d6a0e6c15fe73b362
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-12 17:59:34 +0530

    add the customer entity

---
+++ jpqldemo/src/main/java/com/example/jpqldemo/model/Customer.java
@@ -0,0 +1,25 @@
+package com.example.jpqldemo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int custid;
+    @Column(name = "name")
+    private String custname;
+
+}
