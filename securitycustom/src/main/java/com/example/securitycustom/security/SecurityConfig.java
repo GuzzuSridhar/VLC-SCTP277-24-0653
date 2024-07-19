cb4f0a225e88eb0fd4f8552ea476604440004d10
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-19 16:47:59 +0530

    add custom sec

--- securitycustom/src/main/java/com/example/securitycustom/security/SecurityConfig.java
+++ securitycustom/src/main/java/com/example/securitycustom/security/SecurityConfig.java
@@ -15,41 +15,40 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    // method / bean to create users and their roles
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails userAdmin = User.withUsername("Admin")
-                .password(encoder.encode("admin"))
-                .roles("ADMIN")
-                .build();
-        UserDetails userAlex = User.withUsername("Alex")
-                .password(encoder.encode("pass"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(userAdmin, userAlex);
-    }
+        // method / bean to create users and their roles
+        @Bean
+        public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+                UserDetails userAdmin = User.withUsername("Admin")
+                                .password(encoder.encode("admin"))
+                                .roles("ADMIN")
+                                .build();
+                UserDetails userAlex = User.withUsername("Alex")
+                                .password(encoder.encode("pass"))
+                                .roles("USER")
+                                .build();
+                return new InMemoryUserDetailsManager(userAdmin, userAlex);
+        }
 
-    // creating an instance of the password encryption service
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        // creating an instance of the password encryption service
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    // method / bean to create the endpoint security control
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth ->
-                // auth.requestMatchers("")
-                // .permitAll()
-                auth.requestMatchers(("/"))
-                        .authenticated())
-                .formLogin(
-                        fl -> fl.successForwardUrl("/"))
-                .logout(
-                        lo -> lo.logoutSuccessUrl("/login"));
-        return http.build();
+        /* method / bean to create the endpoint security control */
 
-    }
+        @Bean
+        public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+                        throws Exception {
+                http
+                                .authorizeHttpRequests(
+                                                auth -> auth.requestMatchers("/", "/sample", "/home").authenticated())
+                                .formLogin(
+                                                fl -> fl.successForwardUrl("/"))
+                                .logout(
+                                                lo -> lo.logoutSuccessUrl("/login"));
+                return http.build();
+
+        }
 
 }
